c52d32ad3c53caec024305fc3d348af1d31b26ca
nabaraj-bhandari <nabarajbhandari2005@gmail.com>
2025-05-08 19:57:01 +0545

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Login from './pages/Login'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Login />
     </>
   )
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+    <input type="text" name="firstName" id="firstName" />
+    <input type="text" name="lastName" id="lastName" />
+    </>
+  )
+}
+
+export default Login
